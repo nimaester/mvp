4a00d86279d939c5e185c86d2499bc133cdd3d00
Nim Tayo <nimaester@gmail.com>
2023-01-04 03:21:04 -0800

    updates

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const db = require("../database");
 const parser = require("body-parser");
 const path = require("path");
-
 
 app.use("/", express.static(path.join(__dirname, "../client/dist")));
 app.use(parser.urlencoded({ extended: false }));
